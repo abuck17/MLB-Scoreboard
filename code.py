f2ec025d7da1b286c256356771ea200d0198b8aa
Andrew Buckley <abuck17@gmail.com>
2023-04-19 10:30:48 -0400

    Adding mlb api submodule and base methods for circuit python

---
+++ code.py
@@ -0,0 +1,57 @@
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+
+team = "Phillies"
+
+# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
+# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
+# source control.
+# pylint: disable=no-name-in-module,wrong-import-order
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+
+# Initialize a requests object with a socket and esp32spi interface
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+# Lets make a simple API communication class
+class StatsMLB:
+    def __init__(self, team):
+
+        self.team = team
+
+        self.base_url = "https://statsapi.mlb.com/api/v1"
+        self.schedule_endpoint = self.base_url + "/schedule"
+
+    def get_current_schedule(self):
+
+        response = requests.get(self.schedule_endpoint)
+        print(response.json())
+
+
+mlb = StatsMLB(team)
+mlb.get_current_schedule()
